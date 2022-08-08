a78e407571f9257611b86bb70d101a24dd422f77
Rabeb-Ch <nassimbengana@Mac-mini-alkhas-b-Nassim-2.local>
2022-08-08 18:30:11 +0100

    add framework

---
+++ WeatherAPIComponent/WeatherAPIComponent/WeatherAPIComponent.h
@@ -0,0 +1,18 @@
+//
+//  WeatherAPIComponent.h
+//  WeatherAPIComponent
+//
+//  Created by Nassim Bengana on 8/8/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherAPIComponent.
+FOUNDATION_EXPORT double WeatherAPIComponentVersionNumber;
+
+//! Project version string for WeatherAPIComponent.
+FOUNDATION_EXPORT const unsigned char WeatherAPIComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherAPIComponent/PublicHeader.h>
+
+
